2398f13264ec3bbcc10523acdb498779f2455eb0
DrowzyOtter <leon.fatout@gmail.com>
2025-11-17 21:41:23 +0100

    fitness fct fonctionnelle et affichage simple

--- TIPE-continue.py
+++ TIPE-continue.py
@@ -45,7 +45,7 @@ def pioche_couple_parmi_un_intervalle (minimum,maximum,nb_a_pioché) : #l'interv
 ###generateur à points équidistants par liste d'angles (modèle 1)
 """varables"""
 nb_individu = 20
-dx = 1
+dx = 2
 nb_segment = 5
 nb_génération = 100
 génération_actuelle = 0
@@ -65,6 +65,7 @@ def angles_a_forme (Langles,dx) :
     x , y = 0 , 0
     fap = [(x,y)]
     orientation = 0
+    #print("Langles",Langles)
     for angle in Langles :
         orientation += angle
         x , y = x + dx * sin(orientation*pi/180) , y + dx * cos(orientation*pi/180)
@@ -119,24 +120,27 @@ def création_positions_évaluées_aléatoires (nb_positions_évaluées,Lforet) 
     return positions_évaluées
 
 def création_positions_évaluées_équiréparti (nb_x0,nb_orientations,Lforet) :
-    x0_évalués = liste_équirépartie(-Lforet/2+0.05,Lforet/2-0.05,nb_x0)
-    orientations_évaluées = liste_équirépartie(-179,179,nb_orientations)
+    x0_évalués = liste_équirépartie(-Lforet/2+0.5,Lforet/2-0.5,nb_x0)
+    orientations_évaluées = liste_équirépartie(-360 * (1 - 1/nb_orientations/2),360 * (1 - 1/nb_orientations/2),nb_orientations) #/!\ à fixer
     return x0_évalués, orientations_évaluées
 
 def fitness (individu,dx) :
+    #nb_segment = len(individu)
     nb_x0 = 10
     nb_orientations = 6
-    #x_ind , y_ind = angles_a_forme(individu,dx)
+    xy_ind = angles_a_forme(individu,dx)
     score = nb_x0 * nb_orientations
     x0_évalués, orientations_évaluées = création_positions_évaluées_équiréparti (nb_x0,nb_orientations,Lforet)
     for x0 in x0_évalués :
         for orientation in orientations_évaluées :
-            inside = []
-            for x,y in angles_a_forme(individu,dx) :
-                inside.append(appartenance_Zalgaller(Lforet,x0,orientation,x,y))
-            if False in inside :
+            dedans = []
+            #for x,y in angles_a_forme(individu,dx) :
+            for (x,y) in xy_ind :
+                dedans.append(appartenance_Zalgaller(Lforet,x0,orientation,x,y))
+            if False in dedans :
                 score -= 1
     return score
+
 
 ###méthode 1 bis : tri du couple (f.score,indiv)
 def fusion (c1,c2) :
@@ -177,7 +181,7 @@ def tri_fusion (couples) :
 
 def évaluation_et_tri (population,dx):
     coût_associé = []
-    for individu in range(len(population)) :
+    for individu in population :
         coût_associé.append((fitness(individu,dx),individu))
     return tri_fusion(coût_associé)
 
@@ -213,8 +217,112 @@ def séléction (pop_triée,coût_trié,sigma_mutation) :
 """print(appartenance_Zalgaller(Lforet,0,-97,-2.05,-2.42))
 print(sin(pi/2))"""
 
-
 population = création_pop(nb_individu,nb_segment)
-print(population)
+#print(population)
+#print([x for x,y in angles_a_forme(population[0],dx)])
 pop_triée, coût_trié = couples_à_listes(évaluation_et_tri(population,dx))
-print(pop_triée, coût_trié)
+print(coût_trié)
+
+###-------------------------------###
+###         visualisation         ###
+###-------------------------------###
+
+from tkinter import *
+
+def gradient_color(t):
+    """
+    Renvoie la couleur hexadécimale correspondant à t ∈ [0, 1].
+    """
+    # Points du gradient
+    A = (20, 110, 145)
+    B = (87, 199, 133)
+    C = (237, 221, 83)
+
+    if t <= 0.67:
+        u = t / 0.67
+        r = A[0] + u * (B[0] - A[0])
+        g = A[1] + u * (B[1] - A[1])
+        b = A[2] + u * (B[2] - A[2])
+    else:
+        u = (t - 0.67) / (1 - 0.67)
+        r = B[0] + u * (C[0] - B[0])
+        g = B[1] + u * (C[1] - B[1])
+        b = B[2] + u * (C[2] - B[2])
+
+    return f"#{int(r):02x}{int(g):02x}{int(b):02x}"
+
+# Création de la fenêtre principale
+hauteur = 400
+largeur = 600
+fenêtre = Tk()
+fenêtre.title("évaluation du fitness score simplifiée")
+fenêtre.geometry("600x400")
+fenêtre.configure(bg="#020618")
+#moncanva = Canvas(fenêtre)
+moncanva = Canvas(fenêtre,width=largeur,height=hauteur, bg="#020618",borderwidth=0,highlightthickness=0)
+moncanva.place(relx=0.5, rely=0.5, anchor='center')
+fenêtre.update_idletasks()
+
+
+def convertisseur_affine_x (x) :
+    #return (544-15)/(20+10)*(x+10) + 15
+    #return x*largeur/(4*Lforet)
+    largeur_canva = moncanva.winfo_width()
+    return largeur_canva// 2 + x / (Lforet * 2.5) * largeur_canva
+def convertisseur_affine_y (y) :
+    #return (65-595)/(15+15)*(y+15) + 595
+    #return y*hauteur/(4*Lforet)
+    hauteur_canva = moncanva.winfo_height()
+    largeur_canva = moncanva.winfo_width()
+    return hauteur_canva // 2 + y / (Lforet * 2.5) * largeur_canva
+
+def nv_point (x,y,tx):
+    R = 4
+    X = convertisseur_affine_x(x)
+    Y = convertisseur_affine_y(y)
+    return moncanva.create_oval(X-R,Y-R,X+R,Y+R,fill=gradient_color(tx))
+
+def nv_ligne (x0,y0,x1,y1,tx) :
+    épaisseur = 2
+    if tx == 2 :
+        couleur = "white"
+    else :
+        couleur = gradient_color(tx)
+    X0 = convertisseur_affine_x(x0)
+    Y0 = convertisseur_affine_y(y0)
+    X1 = convertisseur_affine_x(x1)
+    Y1 = convertisseur_affine_y(y1)
+    return moncanva.create_line(X0,Y0,X1,Y1,fill=couleur)
+
+#moncanva.create_oval(100,100,200,200,fill="red")
+
+def afficher_individu (individu,dx,x0,orientation,tx) : #matrice de rotation cachée
+    xy_ind = angles_a_forme(individu,dx)
+    for i in range (len(xy_ind)) :
+        x , y = xy_ind[i]
+        xy_ind[i] = x*cos(orientation) - y*sin(orientation) + x0 , x*sin(orientation) + y*cos(orientation)
+    for i in range (1,len(xy_ind)) :
+        nv_ligne(xy_ind[i-1][0],xy_ind[i-1][1],xy_ind[i][0],xy_ind[i][1],tx)
+    nv_point(x0,0,tx)
+
+
+def fitness_affichage (individu,dx,Lforet) :
+    #nb_segment = len(individu)
+    nv_ligne(-Lforet/2,-moncanva.winfo_height()//2,-Lforet/2,moncanva.winfo_height()//2,2)
+    nv_ligne(Lforet/2,-moncanva.winfo_height()//2,Lforet/2,moncanva.winfo_height()//2,2)
+    nb_x0 = 5
+    nb_orientations = 5
+    x0_évalués, orientations_évaluées = création_positions_évaluées_équiréparti (nb_x0,nb_orientations,Lforet)
+    tx = 0
+    for x0 in x0_évalués :
+        for orientation in orientations_évaluées :
+            #print(x0,orientation)
+            afficher_individu (individu,dx,x0,orientation,tx)
+        tx += 1/(nb_x0)
+
+
+fitness_affichage(pop_triée[0],dx,Lforet)
+print (moncanva.winfo_width())
+
+moncanva.pack(expand=True)
+fenêtre.mainloop()
